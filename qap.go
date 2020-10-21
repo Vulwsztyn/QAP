02aac0005a60b60aaa5d788e8f5cf4f92c07e2aa
Luker <luker.plus@gmail.com>
2020-10-21 11:04:16 +0200

    choose two function

--- qap.go
+++ qap.go
@@ -30,16 +30,37 @@ func genMatrix(size int, maxRange int)(matrix IntMat){
 	return
 }
 
+func permutateIndexes(array *[]int)(res []int){
+	res = rand.Perm(len(*array))
+	return
+}
+
+func chooseTwo(array *[]int)(a int, b int){
+	a = rand.Intn(len(*array))
+	b = rand.Intn(len(*array))
+	for a==b{
+		b = rand.Intn(len(*array))
+	}
+	return
+}
+
 func main() {
 	var timeSplits []int64
 	size := 10
 	maxRange := 10
 	start := time.Now()
+
 	m1 := genMatrix(size, maxRange)
 	m2 := genMatrix(size, maxRange)
+
 	stop := time.Since(start)
 	timeSplits = append(timeSplits,stop.Microseconds())
+
 	fmt.Println(matrix2String(m1))
 	fmt.Println(matrix2String(m2))
+
 	fmt.Println(timeSplits)
+
+	fmt.Println(permutateIndexes(&m1[0]))
+	fmt.Println(chooseTwo(&m1[0]))
 }
