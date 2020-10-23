c13ec3544f82b5333fc1be0d948145bc22e06db6
Luker <luker.plus@gmail.com>
2020-10-23 18:39:44 +0200

    fileReader + instances

--- qap.go
+++ qap.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-const defaultSize = 10
+const defaultSize = 26
 
 type IntMat [defaultSize][defaultSize]int
+
+func fileReader(fileName string)(m1 IntMat, m2 IntMat){
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	rowNum:=0
+	whichMatrix:=1
+	scanner := bufio.NewScanner(f)
+
+	scanner.Scan()
+	instanceSize, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+
+	for scanner.Scan() {
+		row := strings.Fields(strings.TrimSpace(scanner.Text()))
+		if len(row)<instanceSize {
+			continue
+		}
+		for i,v := range row{
+			if whichMatrix==1 {
+				m1[rowNum][i], _ = strconv.Atoi(v)
+			} else {
+				m2[rowNum][i], _ = strconv.Atoi(v)
+			}
+		}
+		rowNum++
+		if rowNum>=instanceSize{
+			rowNum=0
+			whichMatrix++
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
 func (t IntMat) String() string {
 	s := ""
@@ -80,4 +129,6 @@ func main() {
 		randomPermutation(999999)
 	})
 	fmt.Println(timeOfGeneration)
+
+	fmt.Println(fileReader("instances/bur26a.dat"))
 }
