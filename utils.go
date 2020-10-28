7a00236c5afa32b2217c11dd4f3e1c3bc2c336ce
Luker <luker.plus@gmail.com>
2020-10-28 12:43:10 +0100

    steepest (#11)
    * steepest
    
    * fix
    
    * do while
    
    * better neigbourhoods
    
    * createNeighbours simplified
    
    Co-authored-by: Artur Mostowski <artur.mostowski@currency-one.com>

--- utils.go
+++ utils.go
@@ -8,3 +8,15 @@ func measureTime(fn func()) int64 {
 	stop := time.Since(start)
 	return stop.Microseconds()
 }
+
+func min(a []int) (min int, minIndex int) {
+	min = a[0]
+	minIndex = 0
+	for index, value := range a {
+		if value < min {
+			min = value
+			minIndex = index
+		}
+	}
+	return min, minIndex
+}
