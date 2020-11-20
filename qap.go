33cec878a4468ab02070470211c9fe9d4f7582dd
Artur Mostowski <artur.mostowski@currency-one.com>
2020-11-20 19:36:55 +0100

    distance

--- qap.go
+++ qap.go
@@ -205,6 +205,16 @@ func randomWalk(assignment Assignment, timeLimit int64, m1, m2 IntMat) (Assignme
 	return bestAssignment, bestCost, stepCount, stop.Microseconds()
 }
 
+func distance(assignment Assignment, assignment2 Assignment) (distance float64) {
+	for i, v := range assignment {
+		if assignment2[i] != v {
+			distance++
+		}
+	}
+	distance /= defaultSize
+	return
+}
+
 func measureTime(filename string, times int) {
 	m1, m2, _ := fileReader("instances/" + filename + ".dat")
 	var SArray, GArray, HArray, RWArray, RArray [][5]int
@@ -254,4 +264,6 @@ func main() {
 	//fmt.Println(assignmentRW, costRW, stepsRW, timeRW)
 
 	measureTime(instances[5], 10)
+	fmt.Println(distance(Assignment{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, Assignment{0,1,2, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3,19}))
+	fmt.Println(distance(Assignment{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, Assignment{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}))
 }
